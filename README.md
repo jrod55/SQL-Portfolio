64a4bbc9abe408a9c22ec8477a2f271e3641adc4
Jonas Rodriguez <jorod550@gmail.com>
2023-12-16 13:29:21 -0600

    Included extremes query

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Contains the csv files used to populate the MySQL db.
 ### scripts:
 1_create_maize_example_datadb.sql: Creates the db if it does not exist.<br>
 2_filter_populations_join.sql: Filters the data for populations 5-10 and performs a full outer join of the data.<br>
+3_categorize_top_bottom.sql: Filter for the top and bottom 10% of values from each table return the result in 'long' format with top and bottom categories labeled.<br>
 
 ### results:
 Contains output files (saved as .csv files) from SQL query and aggregation scripts. The names of the files correspond to the script used.
---
+++ scripts/3_categorize_top_bottom.sql
@@ -0,0 +1,36 @@
+SELECT 'CL-blue' as trait, Geno_Code, pop, `CL-blue` as value, 
+       CASE WHEN percentile_rank = 1 THEN 'bottom' ELSE 'top' END as top_bottom_label
+FROM (
+    SELECT 
+        *,
+        NTILE(10) OVER (ORDER BY `CL-blue`) AS percentile_rank
+    FROM 
+        maize_example_data.nam_blues_cl
+) AS ranked
+WHERE percentile_rank = 1 OR percentile_rank = 10
+
+UNION ALL
+
+SELECT 'SL-blue' as trait, Geno_Code, pop, `SL-blue` as value, 
+       CASE WHEN percentile_rank = 1 THEN 'bottom' ELSE 'top' END as top_bottom_label
+FROM (
+    SELECT 
+        *,
+        NTILE(10) OVER (ORDER BY `SL-blue`) AS percentile_rank
+    FROM 
+        maize_example_data.nam_blues_sl
+) AS ranked
+WHERE percentile_rank = 1 OR percentile_rank = 10
+
+UNION ALL
+
+SELECT 'KRN-blue' as trait, Geno_Code, pop, `KRN-blue` as value, 
+       CASE WHEN percentile_rank = 1 THEN 'bottom' ELSE 'top' END as top_bottom_label
+FROM (
+    SELECT 
+        *,
+        NTILE(10) OVER (ORDER BY `KRN-blue`) AS percentile_rank
+    FROM 
+        maize_example_data.nam_blues_krn
+) AS ranked
+WHERE percentile_rank = 1 OR percentile_rank = 10;
